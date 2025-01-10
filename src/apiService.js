6657e58fbe105af39ae700710c8446f25cab8521
Israel Espin <israel.espin92@gmail.com>
2025-01-10 16:34:57 +0100

    correcciones

--- src/apiService.js
+++ src/apiService.js
@@ -1,3 +1,7 @@
+export function apiService() {
+  document.getElementById("root").innerHTML = ("#title");
+}
+
 fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
   .then(res => res.json())
   .then(response => {
