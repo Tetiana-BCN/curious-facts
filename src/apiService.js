6dc2117b1bd99cefffbd6e14012164e21c4863ea
Israel Espin <israel.espin92@gmail.com>
2025-01-10 15:07:14 +0100

    arreglo archivos js

--- src/apiService.js
+++ src/apiService.js
@@ -1,19 +1,5 @@
-async function getData() {
-    const url = "https://uselessfacts.jsph.pl/api/v2/facts/random";
-
-     const response = await fetch(url);
-     if (!response.ok) {
-       throw new Error(`Response status: ${response.status}`);
-     }
-    
-     const json = await response.json();
-     console.table(json);
-
-
-     } catch (error) {
-       console.error(error.message);
-     }
-
-getData(); 
-
-
+fetch("https://uselessfacts.jsph.pl/api/v2/facts/random")
+  .then(res => res.json())
+  .then(response => {
+    console.log(response)
+  })
